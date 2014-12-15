a314f9446f0cdb8d30adc5307e3426d7f2bc5055
mapimopi <mapimopipi@gmail.com>
2014-12-15 11:31:36 +0400

    Remove subject from body

--- app/models/email.rb
+++ app/models/email.rb
@@ -29,10 +29,7 @@ class Email < ActiveRecord::Base
     self.body = @markdown.render self.body
 
     unless self.no_reply
-      self.body = "<p id='hash'>##{self.key}</p>" << self.body
-    end
-    unless self.subject.blank?
-      self.body = "<h1>#{self.subject}</h1>" << self.body
+      self.body = "<p id='hash'>##{self.key}</p><br/>" << self.body
     end
   end
 end
